2e9a71785d342a2564edd3277e03b09f11a8dbc2
Radhitka <33850562+radhitka@users.noreply.github.com>
2025-05-25 12:03:07 +0700

    first commit

---
+++ src/main/java/com/tor/inventory/SimpleManagementInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.tor.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleManagementInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleManagementInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tor/inventory/SimpleManagementInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tor.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleManagementInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
